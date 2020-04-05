ebefb9dcc5c45c7eab5e9ce06ec7eece9050091c
hishamaru999 <58763968+hishamaru999@users.noreply.github.com>
2020-04-04 20:39:51 -0700

    added the rhododena curve

--- polar_plot.py
+++ polar_plot.py
@@ -96,3 +96,46 @@ ax1 = plt.subplot(111, polar=True)
 
 #Plot the figure
 ax1.plot(theta, curve)
+
+
+# Rosetta pattern
+n = 2001
+f = np.pi
+r = 5.0
+
+# DATA
+theta = np.linspace(0, 20.0*np.pi, n)
+curve = r*np.cos(f*theta)
+
+# Get an axes handle/object
+ax1 = plt.subplot(111, polar=True)
+
+
+#Plot the figure
+ax1.plot(theta, curve)
+
+
+
+# Rosetta patterns (rhodonea curve)
+# r is the number of tree rings
+# f
+n = 1420
+f = np.pi / 180
+r = 2.0 * np.pi
+
+# DATA
+theta = np.linspace(0, 20.0*np.pi, n)
+curve = r*np.cos(f*theta)
+curve2 = r*np.sin(f*theta)
+
+# Get an axes handle/object
+ax1 = plt.subplot(111, polar=True)
+
+
+#Plot the figure
+ax1.plot(theta, curve, color='xkcd:salmon', label='pink rose curve')
+ax1.plot(theta, curve2, color='xkcd:azure', label='blue rose curve')
+plt.legend()
+plt.title('Rose Plots')
+# -1 so 0 and 360 labels don't overlap
+ax1.set_xticks(np.linspace(0, 2.0*np.pi, 17)[:-1])
