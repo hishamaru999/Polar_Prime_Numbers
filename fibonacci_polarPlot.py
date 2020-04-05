2a04c51a823e4d8ccb575eec879178cf47bab6de
hishamaru999 <58763968+hishamaru999@users.noreply.github.com>
2020-04-05 00:05:25 -0700

    Fibonacci numbers

--- fibonacci_polarPlot.py
+++ fibonacci_polarPlot.py
@@ -6,37 +6,31 @@ import matplotlib as mpl
 mpl.style.use('default')
 
 
-# Fibonacci Numbers
-a = 0
-b = 1
-fibList = [a, b]
-while b < 710:
-    a, b = b, a + b
-    fibList.append(b)
-    print(fibList)
+# Program to display the Fibonacci sequence up to n-th term
+
+nterms = int(input("How many terms? "))
+
+# first two terms
+n1, n2 = 0, 1
+count = 0
+fibList = []
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1, ',')
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
+       fibList.append(n1)
 
 
-# Fibonacci Numbers in Polar Plot
-# r is the number of tree rings
-# f
-n = 1420
-f = 3
-r = 2.0 * np.pi
 
-
-# DATA
-theta = np.linspace(0, fibList, n)
-#curve = r*np.cos(f*theta)
-curve2 = r*np.sin(f*theta)
-
-# Get an axes handle/object
-ax1 = plt.subplot(111, polar=True)
-
-
-#Plot the figure
-#ax1.plot(theta, curve, color='xkcd:salmon')
-ax1.plot(theta, curve2, color='xkcd:azure')
-plt.legend()
-plt.title('Rose Plots')
-# -1 so 0 and 360 labels don't overlap
-ax1.set_xticks(np.linspace(0, 2.0*np.pi, 17)[:-1])
